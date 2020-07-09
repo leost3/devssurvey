2fc4e639fbda636c918148cd1de6dce2b030755f
leonardo studart <lstudart@pyx4.com>
2020-07-08 20:09:09 -0400

    initial commit

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Styles from './login.styles.scss'
+
+
+
+
+const Login: React.FC = () => (
+  <div className={Styles.login}>
+    <header className={Styles.header}>
+      <img src="" alt=""/>
+      <h1>4Dev - Enquetes para Programadores</h1>
+    </header>
+    <form action="" className={Styles.form}>
+
+    </form>
+    <footer className={Styles.footer}>
+      
+    </footer>
+  </div>
+)
+
+export default Login
