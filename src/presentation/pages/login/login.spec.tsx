aab34b5d943bd848da0cbb1eb5420df63c1e6276
leonardo studart <lstudart@pyx4.com>
2020-07-10 21:43:41 -0400

    test: ensure login call validation with correct email/password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,39 @@
-import { render, RenderResult } from '@testing-library/react'
+import { Validation } from '@/presentation/protocols/validation'
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderResult
+} from '@testing-library/react'
 import React from 'react'
 
 import Login from './login'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
 }
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
+
 const makeSut = (): SutTypes => {
-  const sut = render(< Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(< Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe(Login, () => {
+  afterEach(cleanup)
   it('should start with initial state', () => {
     const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
@@ -26,5 +46,22 @@ describe(Login, () => {
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('required')
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  it('should call Validationn with correct email', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
+  })
+  it('should call Validationn with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
   })
 })
