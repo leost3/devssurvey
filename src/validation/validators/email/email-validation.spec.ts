af3a82f58d17566ca786956071112bd116c34e44
leonardo studart <lstudart@pyx4.com>
2020-07-13 21:14:48 -0400

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -5,8 +5,14 @@ import { EmailValidation } from './email-validation'
 
 describe('Email', () => {
   it('should return error if email is not valid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate(faker.internet.email())
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
